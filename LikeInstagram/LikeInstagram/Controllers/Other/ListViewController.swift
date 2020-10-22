9e9b9b0482711b4ca8888da8bcbb81bcc261267e
yeojaeng <duwjdtn1@gmail.com>
2020-10-22 17:56:22 +0900

    Update: LoginViewController Layout

--- LikeInstagram/LikeInstagram/Controllers/Other/ListViewController.swift
+++ LikeInstagram/LikeInstagram/Controllers/Other/ListViewController.swift
@@ -14,7 +14,6 @@ class ListViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
---
+++ LikeInstagram/LikeInstagram/Resources/StorageManager.swift
@@ -0,0 +1,18 @@
+//
+//  StorageManager.swift
+//  LikeInstagram
+//
+//  Created by Yeojaeng on 2020/10/22.
+//
+
+import Foundation
+
+import FirebaseStorage
+
+public class StorageManager {
+
+    // Singleton object
+    static let shared = StorageManager()
+
+    // MARK: - Public
+}
